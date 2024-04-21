143da5968898f4e69c39df87bd2a28410e44f76c
APOgamer <adrianpalmaobi23@gmail.com>
2024-04-20 20:13:18 -0500

    Feat: Testing scss

--- tripmateangular/src/app/app.component.ts
+++ tripmateangular/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {ToolbarComponent} from "./components/toolbar/toolbar.component";
+import {WelcomeComponent} from "./components/welcome/welcome.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ToolbarComponent],
+  imports: [RouterOutlet, ToolbarComponent, WelcomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ tripmateangular/src/app/components/welcome/welcome.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {MainContainerComponent} from "../layout/";
+
+@Component({
+  selector: 'app-welcome',
+  standalone: true,
+  imports: [
+    MainContainerComponent
+  ],
+  templateUrl: './welcome.component.html',
+  styleUrl: './welcome.component.scss'
+})
+export class WelcomeComponent {
+
+}
