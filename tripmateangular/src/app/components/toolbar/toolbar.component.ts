264287f9fb623d168251fdd20e809fe551e5b63e
APOgamer <adrianpalmaobi23@gmail.com>
2024-04-21 22:32:34 -0500

    Update: Toolbar

--- tripmateangular/src/app/components/toolbar/toolbar.component.ts
+++ tripmateangular/src/app/components/toolbar/toolbar.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
 import {MatIconButton} from "@angular/material/button";
+import {MatTab, MatTabGroup} from "@angular/material/tabs";
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, MatIconButton],
+  imports: [MatToolbarModule, MatIconModule, MatIconButton, MatTabGroup, MatTab],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss'
 })
