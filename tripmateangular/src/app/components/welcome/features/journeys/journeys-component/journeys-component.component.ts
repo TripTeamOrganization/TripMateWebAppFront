72a083845bef8a14dd481a0d5732df976ad3c926
MesSobble <u201911727@upc.edu.pe>
2024-04-23 23:02:21 -0500

    feat: add folders

---
+++ tripmateangular/src/app/components/welcome/features/journeys/journeys-component/journeys-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-journeys-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './journeys-component.component.html',
+  styleUrl: './journeys-component.component.scss'
+})
+export class JourneysComponentComponent {
+
+}
