e03805d009208cbf369e54674b33c5491f4ab4b9
APOgamer <adrianpalmaobi23@gmail.com>
2024-04-24 16:30:01 -0500

    Add: Structure cards with react

--- tripmateangular/src/app/components/welcome/features/accomodations/accomodations-component.component.ts
+++ tripmateangular/src/app/components/welcome/features/accomodations/accomodations-component.component.ts
@@ -1,13 +1,15 @@
 import {Component} from '@angular/core';
 import {SearchbarComponent} from "../shared/searchbar/searchbar.component";
 import {CardmainComponent} from "../shared/cardmain/cardmain.component";
+import {CardgroupComponent} from "../shared/groups/cardgroup";
 
 @Component({
   selector: 'app-accomodations',
   standalone: true,
   imports: [
     SearchbarComponent,
-    CardmainComponent
+    CardmainComponent,
+    CardgroupComponent
   ],
   templateUrl: './accomodations-component.component.html',
   styleUrl: './accomodations-component.component.scss'
