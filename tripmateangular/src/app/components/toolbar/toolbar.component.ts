c3468a69568544e876fd5212991468e2937489c1
APOgamer <adrianpalmaobi23@gmail.com>
2024-04-25 17:38:59 -0500

    add:validation

--- tripmateangular/src/app/components/toolbar/toolbar.component.ts
+++ tripmateangular/src/app/components/toolbar/toolbar.component.ts
@@ -3,7 +3,7 @@ import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatIconModule } from "@angular/material/icon";
 import { MatIconButton } from "@angular/material/button";
 import {MatTab, MatTabChangeEvent, MatTabGroup, MatTabLink, MatTabNav, MatTabNavPanel} from "@angular/material/tabs";
-import {Router, RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import { MatTabsModule } from '@angular/material/tabs';
 import {routes} from '../../app.routes';
 import {MatTooltip} from "@angular/material/tooltip";
@@ -16,11 +16,28 @@ import {MatTooltip} from "@angular/material/tooltip";
 })
 export class ToolbarComponent implements AfterViewInit {
   @ViewChild(MatTabGroup) tabGroup: MatTabGroup | undefined;
-  Usuario: string='UsuarioExample';
+  Usuario: string = 'UsuarioExample';
 
   constructor(private router: Router) { }
 
   ngAfterViewInit(): void {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd && this.tabGroup) {
+        let currentRoute = event.url.split('/').pop(); // Obtener la última parte de la ruta
+        const tabLabels = ['accommodations', 'flights', 'activities', 'restaurants', 'myjourneys', 'shoppingcart'];
+
+        // Si la última parte de la ruta está vacía (es la ruta raíz), establecerla en 'restaurants'
+        if (!currentRoute) {
+          currentRoute = 'restaurants';
+        }
+
+        const tabIndex = tabLabels.findIndex(label => label === currentRoute);
+        if (tabIndex !== -1) {
+          this.tabGroup.selectedIndex = tabIndex;
+        }
+      }
+    });
+
     if (this.tabGroup) {
       this.tabGroup.selectedTabChange.subscribe((event: MatTabChangeEvent) => {
         const routes = ['accommodations', 'flights', 'activities', 'restaurants', 'myjourneys', 'shoppingcart'];
