17108f0951b561d3d65a8a96eec489876a975233
APOgamer <adrianpalmaobi23@gmail.com>
2024-04-25 04:46:32 -0500

    Add: Prototype accomodation list tracked
    new @for @if

--- tripmateangular/src/app/components/welcome/features/accomodations/accomodations-component.component.ts
+++ tripmateangular/src/app/components/welcome/features/accomodations/accomodations-component.component.ts
@@ -1,7 +1,11 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {SearchbarComponent} from "../shared/searchbar/searchbar.component";
 import {CardmainComponent} from "../shared/cardmain/cardmain.component";
 import {CardgroupComponent} from "../shared/groups/cardgroup";
+import {Accommodation} from "../../../../models/accomodation.interface";
+import {AllapisService} from "../shared/services/allapis.service";
+import {Restaurant} from "../../../../models/restaurant.interface";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-accomodations',
@@ -9,11 +13,51 @@ import {CardgroupComponent} from "../shared/groups/cardgroup";
   imports: [
     SearchbarComponent,
     CardmainComponent,
-    CardgroupComponent
+    CardgroupComponent,
+    NgForOf,
+    NgIf
   ],
   templateUrl: './accomodations-component.component.html',
   styleUrl: './accomodations-component.component.scss'
 })
-export class AccomodationsComponentComponent {
+export class AccomodationsComponentComponent implements OnInit {
+  accomodationData: Accommodation;
+  accomodations: Accommodation[] = [];
+
+  constructor(private apiservice: AllapisService) {
+    this.accomodationData = {} as Accommodation;
+  }
+
+  ngOnInit() {
+    this.getAccommodations();
+  }
+
+  getAccommodations() {
+    this.apiservice.getAccomodations().subscribe(
+      (data: any) => {
+        if (Array.isArray(data)) {
+          this.accomodations = [];
+          data.forEach((accommodation) => {
+            const newAccommodation = new Accommodation(
+              accommodation.nombre,
+              accommodation.puntuacion,
+              accommodation.imagen,
+              accommodation.descripcion,
+              accommodation.precio,
+              accommodation.direccion,
+              accommodation.telefono,
+              accommodation.sitioWeb
+            );
+            this.accomodations.push(newAccommodation);
+            console.log(this.accomodations);
+          });
+        } else {
+          console.error('El formato de datos recibido no es un array.');
+        }
+        console.log(this.accomodations);
+      }
+    );
+  }
+
 
 }
--- tripmateangular/src/app/models/accomodation.interface.ts
+++ tripmateangular/src/app/models/accomodation.interface.ts
@@ -1,10 +1,30 @@
-export interface Accommodation {
-  name: string;
-  rating: string;
-  image: string;
-  description: string;
-  price: string;
-  address: string;
-  phone: string;
-  website: string;
+export class Accommodation {
+  nombre: string;
+  puntuacion: string;
+  imagen: string;
+  descripcion: string;
+  precio: string;
+  direccion: string;
+  telefono: string;
+  sitioWeb: string;
+
+  constructor(
+    nombre: string,
+    puntuacion: string,
+    imagen: string,
+    descripcion: string,
+    precio: string,
+    direccion: string,
+    telefono: string,
+    sitioWeb: string
+  ) {
+    this.nombre = nombre;
+    this.puntuacion = puntuacion;
+    this.imagen = imagen;
+    this.descripcion = descripcion;
+    this.precio = precio;
+    this.direccion = direccion;
+    this.telefono = telefono;
+    this.sitioWeb = sitioWeb;
+  }
 }
