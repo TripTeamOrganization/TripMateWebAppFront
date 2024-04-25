7a6ded35069c9cc6ad520a8a555a134cf8f39717
MesSobble <u201911727@upc.edu.pe>
2024-04-24 21:51:57 -0500

    feat: add password inputs

---
+++ tripmateangular/src/app/components/welcome/features/shared/password-imputs/password-imputs.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+export interface InputPassword {
+  title: string;
+  placeholder: string;
+
+}
+@Component({
+  selector: 'app-password-imputs',
+  standalone: true,
+  imports: [MatFormFieldModule,
+    MatInputModule],
+  templateUrl: './password-imputs.component.html',
+  styleUrl: './password-imputs.component.scss'
+})
+export class PasswordImputsComponent {
+  @Input()
+  inputData: InputPassword | undefined;
+}
