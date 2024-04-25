4b3c7ad646db5c27d38bbd7b1331880b43d2b9ef
MesSobble <u201911727@upc.edu.pe>
2024-04-24 21:35:11 -0500

    feat: add inputs

---
+++ tripmateangular/src/app/components/welcome/features/shared/imputs/imputs.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+export interface InputInfo {
+  title: string;
+  placeholder: string;
+
+}
+@Component({
+  selector: 'app-imputs',
+  standalone: true,
+  imports: [MatFormFieldModule,
+    MatInputModule],
+  templateUrl: './imputs.component.html',
+  styleUrl: './imputs.component.scss'
+})
+export class ImputsComponent {
+  @Input()
+  inputData: InputInfo | undefined;
+}
