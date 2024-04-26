699fdb4b603673e5aa9dc9a52b86ece844aac6c1
Leonel-Alfaro <u20201a930@upc.edu.pe>
2024-04-26 18:41:25 -0500

    Addt: dropdown

--- tripmateangular/src/app/components/welcome/features/shared/main-filter/dropdown-card/dropdown-card.component.ts
+++ tripmateangular/src/app/components/welcome/features/shared/main-filter/dropdown-card/dropdown-card.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 @Component({
   selector: 'app-dropdown-card',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatSelectModule],
   templateUrl: './dropdown-card.component.html',
   styleUrl: './dropdown-card.component.scss'
 })
 export class DropdownCardComponent {
-
+  @Input() title: string = '';
+  @Input() option1: string = '';
+  @Input() option2: string = '';
+  @Input() option3: string = '';
+  @Input() option4: string = '';
+  @Input() option5: string = '';
+  selected: any;
 }
