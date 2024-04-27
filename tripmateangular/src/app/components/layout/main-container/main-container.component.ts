c65bd3f340f76a84cc217a8453e4fe4a8b7a7be7
MesSobble <u201911727@upc.edu.pe>
2024-04-27 02:29:02 -0500

    feat: add conditionals

--- tripmateangular/src/app/components/layout/main-container/main-container.component.ts
+++ tripmateangular/src/app/components/layout/main-container/main-container.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-container',
@@ -8,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrl: './main-container.component.scss'
 })
 export class MainContainerComponent {
-
+  constructor(private router: Router) { }
+  isRegisteredRoute() {
+    if (this.router.url === '/register') {
+      console.log('Register route');
+      return true;
+    } 
+    
+    else {
+    return false;
+    }
+  }
 }
