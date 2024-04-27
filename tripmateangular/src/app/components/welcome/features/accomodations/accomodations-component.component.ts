19acfb12d05ba8be400155180bb1bc1732e346d0
paolomv02 <u202010039@upc.edu.pe>
2024-04-27 00:40:12 -0500

    fix: edit accomodations imports

--- tripmateangular/src/app/components/welcome/features/accomodations/accomodations-component.component.ts
+++ tripmateangular/src/app/components/welcome/features/accomodations/accomodations-component.component.ts
@@ -5,17 +5,19 @@ import {CardgroupComponent} from "../shared/groups/cardgroup";
 import {Accommodation} from "../../../../models/accomodation.interface";
 import {AllapisService} from "../shared/services/allapis.service";
 import {NgForOf, NgIf} from "@angular/common";
+import {MainFilterComponent} from "../shared/main-filter/main-filter.component";
 
 @Component({
   selector: 'app-accomodations',
   standalone: true,
-  imports: [
-    SearchbarComponent,
-    CardmainComponent,
-    CardgroupComponent,
-    NgForOf,
-    NgIf
-  ],
+    imports: [
+        SearchbarComponent,
+        CardmainComponent,
+        CardgroupComponent,
+        NgForOf,
+        NgIf,
+        MainFilterComponent
+    ],
   templateUrl: './accomodations-component.component.html',
   styleUrl: './accomodations-component.component.scss'
 })
