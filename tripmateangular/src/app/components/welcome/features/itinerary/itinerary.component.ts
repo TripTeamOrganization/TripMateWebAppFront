ed46a0ea7d9e10a81f01a3000c116c1bb02a9cc0
Leonel-Alfaro <u20201a930@upc.edu.pe>
2024-04-28 15:07:44 -0500

    feat: add Itinerary

--- tripmateangular/src/app/components/welcome/features/itinerary/itinerary.component.ts
+++ tripmateangular/src/app/components/welcome/features/itinerary/itinerary.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import {SearchbarComponent} from "../shared/searchbar/searchbar.component";
+import {MatTab, MatTabGroup} from "@angular/material/tabs";
+import {RouterLink, RouterLinkActive} from "@angular/router";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-itinerary',
   standalone: true,
-  imports: [],
+  imports: [
+    SearchbarComponent,
+    MatTab,
+    MatTabGroup,
+    RouterLink,
+    RouterLinkActive,
+    NgForOf
+  ],
   templateUrl: './itinerary.component.html',
   styleUrl: './itinerary.component.scss'
 })
 export class ItineraryComponent {
 
+  daysOfWeek = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
+  selectedDay: number | null = null;
+
+  selectDay(index: number) {
+    this.selectedDay = index;
+  }
 }
