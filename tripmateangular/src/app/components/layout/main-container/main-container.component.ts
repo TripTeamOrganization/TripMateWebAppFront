baea251c91a619433404a33d3fc4769d676378e3
APOgamer <adrianpalmaobi23@gmail.com>
2024-04-27 21:33:26 -0500

    Add: signin

--- tripmateangular/src/app/components/layout/main-container/main-container.component.ts
+++ tripmateangular/src/app/components/layout/main-container/main-container.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import {RegisterComponent} from "../../welcome/features/register/register.component";
+import { SigninComponent } from '../../welcome/features/signin/signin.component';
 
 @Component({
   selector: 'app-main-container',
   standalone: true,
   imports: [
-    RegisterComponent
+    RegisterComponent,
+    SigninComponent
   ],
   templateUrl: './main-container.component.html',
   styleUrl: './main-container.component.scss'
@@ -18,7 +20,16 @@ export class MainContainerComponent {
       return true;
     }
     else {
-    return false;
+      return false;
+    }
+  }
+
+  isSignInRoute() {
+    if (this.router.url === '/signin') {
+      return true;
+    }
+    else {
+      return false;
     }
   }
 }
