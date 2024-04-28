a1619f9864abd649735e8ceedfce2f225ecd0816
paolomv02 <u202010039@upc.edu.pe>
2024-04-28 00:02:08 -0500

    feat: import modules

--- tripmateangular/src/app/components/welcome/features/userview/userview.component.ts
+++ tripmateangular/src/app/components/welcome/features/userview/userview.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'app-userview',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './userview.component.html',
   styleUrl: './userview.component.scss'
 })
