18f0508552d4d7dd6e6c08f61e8ec1e02d68284d
paolomv02 <u202010039@upc.edu.pe>
2024-04-29 12:48:06 -0500

    feat: create plans

---
+++ tripmateangular/src/app/components/welcome/features/plans/plans.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {MatCardModule} from "@angular/material/card";
+import {CardPlanComponent} from "../../../../../public/shared/card-plan/card-plan.component";
+
+@Component({
+  selector: 'app-plans',
+  standalone: true,
+  imports: [MatCardModule, CardPlanComponent],
+  templateUrl: './plans.component.html',
+  styleUrl: './plans.component.scss'
+})
+export class PlansComponent {
+
+}
