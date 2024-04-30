a77497a29df5bfeef08d93e8a689535b3a31f203
Jeremy Joel Quispe Andia <u202216279@upc.edu.pe>
2024-04-29 22:23:18 -0500

    feat: added redirection to menu

--- tripmateangular/src/app/components/welcome/features/payment-succesfully/payment-succesfully.component.ts
+++ tripmateangular/src/app/components/welcome/features/payment-succesfully/payment-succesfully.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {MatButton} from "@angular/material/button";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-payment-succesfully',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButton
+  ],
   templateUrl: './payment-succesfully.component.html',
   styleUrl: './payment-succesfully.component.scss'
 })
 export class PaymentSuccesfullyComponent {
+  constructor(private router: Router) {
+  }
 
+  onSubmitButton(){
+    this.router.navigate(['accommodations']);
+  }
 }
