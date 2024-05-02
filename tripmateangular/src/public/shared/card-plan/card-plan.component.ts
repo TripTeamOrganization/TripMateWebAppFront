ee0ecae66d4e48a1fa8fe6e9efd9e2a6440f3392
MesSobble <u201911727@upc.edu.pe>
2024-05-02 01:55:55 -0500

    feat: add responsive

--- tripmateangular/src/public/shared/card-plan/card-plan.component.ts
+++ tripmateangular/src/public/shared/card-plan/card-plan.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import {MatCardModule} from "@angular/material/card";
 import {MatButtonModule} from "@angular/material/button";
+import {SearchbarComponent} from "../searchbar/searchbar.component";
 
 @Component({
   selector: 'app-card-plan',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, SearchbarComponent],
   templateUrl: './card-plan.component.html',
   styleUrl: './card-plan.component.scss'
 })
@@ -17,7 +18,6 @@ export class CardPlanComponent {
   @Input() feature1: string = '';
   @Input() feature2: string = '';
   @Input() feature3: string = '';
-
   @Output() onSelectPlan = new EventEmitter<void>();
 
   onButtonClick() {
