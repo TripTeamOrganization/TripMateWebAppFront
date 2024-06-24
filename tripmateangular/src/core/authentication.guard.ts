6f154e5c59d4f4045593c756df74ab1643fcd0b9
APOgamer <adrianpalmaobi23@gmail.com>
2024-06-23 19:19:32 -0500

    Fix: Routing fix to signin

--- tripmateangular/src/core/authentication.guard.ts
+++ tripmateangular/src/core/authentication.guard.ts
@@ -9,6 +9,6 @@ export const AuthGuard: CanActivateFn = (route, state) => {
   if(authService.isAuthenticated()) {
     return true;
   } else {
-    return router.navigate(['/accommodations']);
+    return router.navigate(['/signin']);
   }
 };
